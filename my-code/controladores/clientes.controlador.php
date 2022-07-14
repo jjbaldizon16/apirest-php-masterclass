670e209a16026d115238a17a7617ce14c30c0ddf
jjbaldizon16 <baldizon.cv16@gmail.com>
2022-07-14 09:02:28 -0600

    update
    update

--- my-code/controladores/clientes.controlador.php
+++ my-code/controladores/clientes.controlador.php
@@ -55,8 +55,50 @@ class ControladorClientes {
 			return;
 		}
 
-           $json = array("detalle"=>"Registro guardado");
+          /**Validar que el email no este repetido */
+
+           $clientes = ModeloCLientes::index("clientes");
+
+           foreach($clientes as $key => $value){
+
+             if($value["email"] == $datos["email"]){
+
+           $json = array("status"=>404,
+                         "detalle"=>"Email repetido");
            echo json_encode($json, true);
+           return;
+
+             }
+
+           }
+
+          /*=============================================
+		Generar credenciales del cliente
+		=============================================*/
+
+		$id_cliente = str_replace("$", "a", crypt($datos["nombre"].$datos["apellido"].$datos["email"], '$2a$07$afartwetsdAD52356FEDGsfhsd$'));
+	
+		$llave_secreta = str_replace("$", "o", crypt($datos["email"].$datos["apellido"].$datos["nombre"], '$2a$07$afartwetsdAD52356FEDGsfhsd$'));
+           
+            
+          /**Llevar datos al modelo */
+          
+          $datos = array("nombre"=>$datos["nombre"],
+                    "apellido"=>$datos["apellido"],
+                    "email"=>$datos["email"],
+                    "id_cliente"=>$id_cliente,
+                    "llave_secreta"=>$llave_secreta,
+                    "created_at"=>date('Y-m-d h:i:s'),
+                    "updated_at"=>date('Y-m-d h:i:s')
+                    );  
+          $create = ModeloClientes::create("clientes", $datos);
+          
+          echo '<pre>'; print_r($create); echo '</pre>';
+           return;
+
+          /**Respuesta del modelo */ 
+
+           
 
 
      }
